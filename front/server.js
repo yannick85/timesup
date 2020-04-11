82c69d77f8f412325caa20b347461dad4e9d576f
YannickB <yannick.betou@hardis.Fr>
2020-04-11 17:23:17 +0200

    .

--- front/server.js
+++ front/server.js
@@ -8,6 +8,6 @@ app.get('*', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'dist', 'index.html'))
 })
 
-var port = process.env.PORT || 8080
+var port = process.env.PORT || 5000
 app.listen(port)
 console.log('server started ' + port)
